58cc2277b17a4c8fd2cd50632ae307f828edf4aa
asim <90917263+asimadalat@users.noreply.github.com>
2026-06-06 17:08:58 +0100

    feat: init solution and define project structure

---
+++ src/backend/GridTelemetry.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace GridTelemetry.Core;
+
+public class Class1
+{
+
+}
---
+++ src/backend/GridTelemetry.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace GridTelemetry.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
